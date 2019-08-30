576cb16deeef7b759752666dc649e444810e55bb
Eric Butler <ebutler@milestechnologies.com>
2019-08-30 07:57:41 -0400

    Add project files.

---
+++ EButlerBooks/Models/DbEntities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EButlerBooks.Models
+{
+    public class DbEntities : DbContext
+    {
+        //public DbEntities() : base()
+        //{
+
+        //}
+        public DbEntities(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        DbSet<Book> Books { get; set; }
+    }
+}
---
+++ EButlerBooks/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using EButlerBooks.Models;
+
+namespace EButlerBooks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            Env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Replace connection string tokens
+            var connString = Configuration.GetConnectionString("default");
+            if (connString.Contains("%CONTENTROOTPATH%"))
+            {
+                connString = connString.Replace("%CONTENTROOTPATH%", Env.ContentRootPath);
+            }
+
+
+            services.AddRazorPages();
+            services.AddDbContext<DbEntities>(options =>
+            {
+                options.UseSqlServer(connString);
+            }
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
