115c9bb013e55b5b3fed311779e783ebf60b5f2c
Eric Butler <e.c.butler@outlook.com>
2019-09-03 13:58:31 -0400

    book table

---
+++ EButlerBooks/Migrations/20190903174043_bookFields.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EButlerBooks.Migrations
+{
+    public partial class bookFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Books");
+        }
+    }
+}
--- EButlerBooks/Models/Book.cs
+++ EButlerBooks/Models/Book.cs
@@ -9,5 +9,7 @@ namespace EButlerBooks.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
