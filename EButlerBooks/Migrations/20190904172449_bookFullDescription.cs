48f5a1241978f3758c2196f5875395f05d9c0276
Eric Butler <e.c.butler@outlook.com>
2019-09-04 13:25:22 -0400

    Description

---
+++ EButlerBooks/Migrations/20190904172449_bookFullDescription.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EButlerBooks.Migrations
+{
+    public partial class bookFullDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullDescription",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullDescription",
+                table: "Books");
+        }
+    }
+}
