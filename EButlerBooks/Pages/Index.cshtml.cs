138c74478dbe11a992f14b3947a584d9dabd4c9e
Eric Butler <e.c.butler@outlook.com>
2019-12-16 23:56:51 -0500

    linq update

--- EButlerBooks/Pages/Index.cshtml.cs
+++ EButlerBooks/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using EButlerBooks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace EButlerBooks.Pages
@@ -24,7 +25,9 @@ namespace EButlerBooks.Pages
         public void OnGet()
         {
             // Get the featured book from the app's settings
-            var appSettings = _db.AppSettings.Where(s => s.Id == 1);
+            var appSettings = (from s in _db.AppSettings.Include(a => a.FeaturedBook)
+                               where s.Id == 1
+                               select s).FirstOrDefault();
             //_featuredBook = appSettings?.FeaturedBook;
         }
     }
